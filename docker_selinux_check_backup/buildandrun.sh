beca025e86596fe808ab9483a1850fb787134927
Matthew Kirk <matt.kirkk@rancherfederal.com>
2020-12-02 10:17:53 -0500

    fixed stupid folder

--- docker_selinux_check_backup/buildandrun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Builds a fresh container from the current dir, then runs it
-docker build . -t docker-selinux-check
-
-docker run -it -v /etc/selinux:/etc/selinux -v /etc/hostname:/etc/hostname -p 5000:5000 docker-selinux-check:latest
-
